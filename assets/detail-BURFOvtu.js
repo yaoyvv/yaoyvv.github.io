16af08f32f33ce2d38a8a27851b5857fd037f4a0
yaoyvv <yaoyvv@gmail.com>
2025-08-09 17:38:50 +0800

    Deploy Vue3 frontend

--- assets/detail-BURFOvtu.js
+++
@@ -1,1 +0,0 @@
-import{M as t}from"./index-DfAD_XQf.js";const e={PurchaseList:"/get-purchase-list",ProjectList:"/get-project-list"};function s(){return t.get({url:e.PurchaseList})}function i(){return t.get({url:e.ProjectList})}export{i as a,s as g};
--- assets/index-CaEOJaWR.js
+++
@@ -1,1 +0,0 @@
-import{D as f}from"./index-DfAD_XQf.js";export{f as default};
